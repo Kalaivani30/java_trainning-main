f398e4b95c44cc16c540629b4ff842ef02b5fd43
Kalaivani30 <kalaikailash9467@gmail.com>
2026-06-16 16:17:40 +0530

    changes

--- DSA/List/Arraylist.java
+++ DSA/List/Arraylist.java
@@ -4,8 +4,7 @@ public class Arraylist{
      public static void main(String[] args){
         ArrayList<Integer> a1=new ArrayList<>();//<Object> is used to store any type of data
         a1.add(10);
-        a1.add(20);
-        a1.add(30);//a1.add("thara")
+       
 
         for(int i=0;i<a1.size();i++){
                 System.out.println(a1.get(i));
